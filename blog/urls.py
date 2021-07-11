f97fc1cc4e52fea975492fb52bf5e9ad97f6a681
jcidp <jcidp@hotmail.com>
2021-07-11 10:42:31 -0500

    Created html and templates to display post from database.

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+#The next line imports all the views from the current directory, which is blog.
+from . import views
+
+#We create a url pattern using the path function we imported from django. 
+#The first argument is empty cause we want the base URL to return that view. AKA the URL as is, like the landing page
+#The 2nd arg tells django to display the view called post_list
+#The last arg gives the name post_list to the URL. We want to name each URL.
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
